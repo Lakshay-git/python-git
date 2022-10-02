50df2b60d36927418f63508997cc553ccc60e5af
Lakshay soni <lakshaysoni5252@gmail.com>
2022-10-02 14:34:51 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,38 @@
+#create two variables and assign values of them
+
+#create a third variableand add x and y
+
+#print the sum 
+
+#x = 5 
+#y = 7 
+#print(x + y)
+
+#first = input("enter the first number:")
+#second = input("enter the second number:")
+
+#sum = int(first) + int(second)
+#print("the sum is:" + str(sum))
+
+#x = 5 
+#y = 4 
+
+#print("value of x is:" , x)
+#print("value of y is:" , y)
+
+#temp = x 
+#print("value of x is:" , temp)
+#x = y 
+#print("value of x is:" , x )
+#y = temp 
+#print("value of y is:" , y) 
+
+#name = "tony stark"
+#print(name.replace("tony stark", "ironman"))
+#print(name)
+
+
+old_age = input("enter your old age :")
+new_age = int("old age") + 2
+print(new_age)
+
