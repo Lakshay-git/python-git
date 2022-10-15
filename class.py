736a3c4157c32a704c9d3a3439fa8bccd600f99c
Lakshay soni <lakshaysoni5252@gmail.com>
2022-10-15 13:10:22 +0530

    class

---
+++ class.py
@@ -0,0 +1,30 @@
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} is Even".format(num))
+# else:
+#    print("{0} is Odd".format(num))
+
+#company will give bonus base on following criteria 
+
+#time spent in copany           bonus 
+#     greater than 10 years       10%
+
+#more than 6 and less
+#    than 10                       8%
+
+#   less than 6                    5%
+
+#ask the salary and time spent from the user 
+#print the net bonus amount accordingly 
+
+salary = int(input("enter your current salary"))
+year = int(input("enter your year spend"))
+
+if year >= 10 :
+    print("your net salary with 10 percent bonus- ", salary+salary*10/100)
+
+elif year > 6 <= 10:
+    print("your net salary wit 8 percent bonus-", salary+ salary*8/100)
+
+elif year < 6:
+    print("your net salary with 5 percent bonus-", salary+salary*5/100)
---
+++ hello.py
@@ -0,0 +1,25 @@
+#length = 10 
+#breadth = 20 
+#print ( 'length = ',length,' breadth = ',breadth )
+#area = length * breadth 
+#print ('area of reatangle is = ', area)
+
+#str1 = input ('enter string 1: ')
+#str2 = input ('enter string 2: ')
+#print ('string1 = ',str1)
+#print ('string2 = ',str2)
+
+#print ('please enter the number :')
+#x = input()
+#print (' entered number is: ', x )
+#print  ('type of x is:')
+#print (type(x))
+
+print (' please enter number ')
+num1 = input()
+print (' num1 =',num1)
+print (type(num1))
+print ('converting type of num1 to int')
+num1 = int (num1)
+print(num1)
+print(type(num1))
