c303bc9f4f728cc6fe818950913fa80acd9fff27
Lakshay soni <lakshaysoni5252@gmail.com>
2022-10-18 00:37:02 +0530

    first commit

--- class.py
+++ class.py
@@ -1,30 +1,85 @@
-# num = int(input("Enter a number: "))
-# if (num % 2) == 0:
-#    print("{0} is Even".format(num))
+# # num = int(input("Enter a number: "))
+# # if (num % 2) == 0:
+# #    print("{0} is Even".format(num))
+# # else:
+# #    print("{0} is Odd".format(num))
+
+# #company will give bonus base on following criteria 
+
+# #time spent in copany           bonus 
+# #     greater than 10 years       10%
+
+# #more than 6 and less
+# #    than 10                       8%
+
+# #   less than 6                    5%
+
+# #ask the salary and time spent from the user 
+# #print the net bonus amount accordingly 
+
+# salary = int(input("enter your current salary"))
+# year = int(input("enter your year spend"))
+
+# if year >= 10 :
+#     print("your net salary with 10 percent bonus- ", salary+salary*10/100)
+
+# elif year > 6 <= 10:
+#     print("your net salary wit 8 percent bonus-", salary+ salary*8/100)
+
+# elif year < 6:
+#     print("your net salary with 5 percent bonus-", salary+salary*5/100)
+
+#import random
+
+#print(random.randrange(0,100))
+
+# string = "ABCD"
+# print(string[0])
+# print(len(string))
+
+#string = "hello world"
+#print(len(string))
+
+#in 
+
+# quote = "a quick brown fox jumps over the lazy dog"
+
+# if "quick" not in quote:
+#     print("yes")
 # else:
-#    print("{0} is Odd".format(num))
+#     print("no")
 
-#company will give bonus base on following criteria 
+# a = "hello world"
+# print(a[-5:-1])
 
-#time spent in copany           bonus 
-#     greater than 10 years       10%
+# a = "hello world"
+# print(a.lower())
 
-#more than 6 and less
-#    than 10                       8%
+# a = "    hello world"
+# print(a)
+# print(a.strip())
 
-#   less than 6                    5%
+# a = "hello world"
+# print(a)
+# print(a.replace("hello", "hey"))
 
-#ask the salary and time spent from the user 
-#print the net bonus amount accordingly 
 
-salary = int(input("enter your current salary"))
-year = int(input("enter your year spend"))
+# a = "hello"
+# b = "world"
+# c = a +"  " + b
+# print(c)
 
-if year >= 10 :
-    print("your net salary with 10 percent bonus- ", salary+salary*10/100)
+# xyz = "a quick brown fox jumps\n""over the lazy dog"
+# print(xyz)
 
-elif year > 6 <= 10:
-    print("your net salary wit 8 percent bonus-", salary+ salary*8/100)
+#take a string of length 5 [index 0-4]
+#print first charac 
+#print middle charac
+#print last charac
 
-elif year < 6:
-    print("your net salary with 5 percent bonus-", salary+salary*5/100)
+# string = "ravii"
+# length = len(string)
+# c = (length//2)
+# print(string[0])
+# print(string[c])
+# print(string[-1])
