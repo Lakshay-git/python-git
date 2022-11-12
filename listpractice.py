42e0682b726ce7ccc1da6e8200a77ac205687767
Lakshay soni <lakshaysoni5252@gmail.com>
2022-11-12 22:54:31 +0530

    listpractice.py

---
+++ listpractice.py
@@ -0,0 +1,15 @@
+# colors = ["red","black","red","blue",22]
+# # #colors[2] = "green"
+# # #print(colors[1:4]
+# colors[0:2] = ["yellow"]
+# colors[2:3]=["orange"]
+# print(colors)
+
+colors = ["red","black","red","blue"]
+cars = ["tata","nano","alto"]
+players = ("dhoni","messi","dravid")
+
+#colors.append("lakshay")
+#colors.insert(2,"indigo")
+colors.replace("red","orange")
+print(colors)
