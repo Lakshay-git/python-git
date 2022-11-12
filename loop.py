f801a373f4104082201e110da92ba9d9a704e647
Lakshay soni <lakshaysoni5252@gmail.com>
2022-11-12 22:56:27 +0530

    loop.py

---
+++ loop.py
@@ -0,0 +1,39 @@
+colors = ["red","blue","red","black"]
+cars = ["tata","nano","alto","jeep"]
+
+#in the above list, find value 2, if it is present, 
+#replace it with 200 only update the first occurance
+# num = [1, 4, 6, 2, 10]
+# newlist=[x.lower() for x in cars if x!="NANO"]
+# print(newlist)
+# num.sort(reverse=True)
+# print(num)
+
+#newlist = [x for x in cars  if x != "tata"]
+# .    expression item list  cond
+#print(newlist)
+
+list1 = ["x","y","z"]
+list2 = [1,2,3]
+
+list3 = list1 + list2
+print(list3)
+
+
+
+#print x or x in cars]
+ #newlist = []
+# for i in cars:
+#     if"a" in i:
+#         newlist.append(i)
+# print(newlist)
+
+
+
+
+# for X in range(len cars):
+#     print(cars [X])
+# X = 0
+# while X < len(cars):
+#     print(cars [X])
+#     X+=1
