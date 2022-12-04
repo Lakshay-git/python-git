49b3c0600a0f215aef8fb811afd9120d0fab736e
Lakshay soni <lakshaysoni5252@gmail.com>
2022-12-04 16:47:36 +0530

    tuple.py

---
+++ tupleloop.py
@@ -0,0 +1,16 @@
+# #loops
+
+# myTuple = ("car","boat","bike")
+# # for a in myTuple:
+# #     print(a)
+# i = 0
+# while i < len(myTuple):
+#     print(myTuple[i])
+#     i+=1
+ 
+tuple1 = (1,[6,7],2,3,4,5)
+#a = tuple[1][0]
+#print(a)
+
+tuple1[1][0] = 8
+print(tuple1)
