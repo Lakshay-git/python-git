79c4fc11e72af7c001b7f0c13f4dc4d0385bda48
Lakshay soni <lakshaysoni5252@gmail.com>
2022-12-04 16:55:50 +0530

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+# #dictionary - used to store values in key:value
+
+# myDictionary = {
+#     "name":"nikhil",
+#     "age":"22",
+#     "precent":"80.5"
+
+# }
+# #age = myDictionary.get("age")
+# #keys = myDictionary.values()
+# #items = myDictionary.items()
+
+
+# #orderd
+# #changeable
+# #does not allow duplicate values
+
+# #myDictionary.update({"age":33})
+# #print(myDictionary)
+# #myDictionary.pop("age")
+# #print(myDictionary)
+
+# dict1 = {"one":1,"two":2,"three":3}
+# dict2 = {"four":4,"five":5,"six":6}
+# #merge two dictionaries in one
+# dict1.update(dict2)
+# print(dict1)
+
+dict1 = {
+    "class":{
+        "student":{
+            "name":"xyz",
+            "marks":{
+                "python":100,
+                "web":90
+            }
+        }
+    }
+}
+print("marks")
