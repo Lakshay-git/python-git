a6995bc7bc5cdc3bcdb8127705a33fd94f825d95
Lakshay soni <lakshaysoni5252@gmail.com>
2022-12-07 00:58:47 +0530

    listpractice.py

---
+++ listpractice2.py
@@ -0,0 +1,38 @@
+#list are used store multiple
+#non-homogeneus data in a single variable 
+#orderd
+#changable
+#allows duplicate value 
+#insert() - insert item at specified index 
+#append() - insert item at the end of the list 
+#extend() - it appends elements from one list to another list, it can be  used to append different collection 
+#remove() - remove item from specified index
+#pop()
+#clear() - clears all elements in the list 
+#range(), len()
+#sort() - it will sort the list in asscending order 
+#copy()
+
+
+#List are used store multiple non-homogeneous data in a single variable
+# ordered
+# Changable
+# Allows duplicate value
+# 
+# Insert() - insert item  at specified index
+# Append() - insert item at the end of the list
+# Extend() - it appends elements from one list to another list, it can be used to append different collection
+# Remove() - removes item from a specified index
+# pop()
+# clear() - clears all elements
+# range(), len()
+# sort() - it will sort the list in ascending order
+# copy() 
+
+#tuples store multiple items in a single variable 
+#tuples are unchangable 
+#orderd 
+#use() paranthesis for tuple 
+#allows duplicate value 
+myTuple = ("apple","orange","pinapple")
+print( myTuple[0:])
