64c3c3853f5aa77f5c4cf25aefe58685370cade6
Lakshay soni <lakshaysoni5252@gmail.com>
2022-12-25 20:21:19 +0530

    first commit

---
+++ polymorphism_1.py
@@ -0,0 +1,82 @@
+#print(len("hello world'))
+#print(len([10,20,30]))
+
+# def add(x,y,z = 0):
+#    return x+y+z
+# print(add(3,4))
+# print(add(3,4,5))
+class rect:
+    def calculateArea(self,length, breadth)
+
+
+# class India:
+#     def capital(self):
+#         print("new delhi")
+#     def language(self):
+#         print("hindi")
+#     def type(self):
+#         print("developed")
+
+# class USA:
+#     def capital(self):
+#         print("washington DC")
+#     def language(self):
+#         print("english")
+#     def type(self):
+#         print("developing")
+
+# obj1 = India()
+# obj2 = USA()
+# for i in (obj1, obj2):
+#     i.capital()
+#     i.language()
+#     i.type()
+
+
+# class bird:
+#     def wings(self):
+#         print("bird have two wings")
+#     def eyes(self):
+#         print("bird has two eyes")
+#     def fly(self):
+#         print("most of the birds can fly")
+
+# class sparrow(bird):
+#     def wings(self):
+#         print("")
+#     def eyes(self):
+#         print("")
+#     def fly(self):
+#         print("")
+
+# class ostrich(bird):
+#     pass
+#     #def fly(self):
+#      #   print("ostrich can not fly")
+
+# bird = bird()
+# sparrow = sparrow()
+# ostrich = ostrich()
+
+# bird.eyes()
+# bird.wings()
+# bird.fly()
+# bird.fly()
+
+#create a car class without any variables and methods
+
+# class car:
+#     pass
+
+# #create a child class bus that will inherit 
+
+# class vehicle:
+#     def __init__(self,name,milage):
+#         self.name = name
+#         self.milage = milage
+# class bus(vehicle):
+#     pass
+
+# obj = bus("abc",10)
+# print(obj.milage, obj.name)
+
